0b76cd5f40d2108da10bfc89e8a3bce95ba1835c
sepuckett86 <sepuckett86@gmail.com>
2019-07-18 10:21:34 -0700

    pass studio get by id test

--- test/studios.test.js
+++ test/studios.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
 
 describe('studio routes', () => {
   beforeAll(() => {
@@ -53,7 +55,7 @@ describe('studio routes', () => {
         country: 'United States'
       }
     });
-
+    
     return request(app)
       .get('/api/v1/studios')
       .then(res => {
@@ -64,7 +66,9 @@ describe('studio routes', () => {
         }]);
       });
   });
+
   it('gets studio by id', async() => {
+    // { _id, name, address, films: [{ _id, title }] }
     const studio = await Studio.create({
       name: 'Firefly Studio',
       address: {
@@ -73,13 +77,40 @@ describe('studio routes', () => {
         country: 'United States'
       }
     });
+
+    const actor = await Actor.create({
+      name: 'Leonardo DiCaprio',
+      dob: new Date('November 11, 1974'),
+      pob: 'Hollywood, California'
+    });
+
+    const film = await Film.create({
+      title: 'Awesome Sauce',
+      studio: studio._id,
+      released: 1990,
+      cast: [{
+        role: 'Derrick Strong',
+        actor: actor._id
+      }]
+    });
+
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
         const studioCleaned = JSON.parse(JSON.stringify(studio));
-        expect(res.body).toEqual(studioCleaned);
+        const filmCleaned = JSON.parse(JSON.stringify(film));
+        expect(res.body).toEqual({
+          _id: studioCleaned._id,
+          name: studioCleaned.name,
+          address: studioCleaned.address,
+          films: [{
+            _id: filmCleaned._id,
+            title: filmCleaned.title
+          }]
+        });
       });
   });
+  
   it('deletes studio', async() => {
     const studio = await Studio.create({
       name: 'Firefly Studio',
